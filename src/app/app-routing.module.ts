1e40f5d97492f61904babeb199aab0081666f1fc
PerfectzDev <gamerzperfectz@gmail.com>
2024-01-14 15:57:19 +0800

    Update.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import {AboutUsComponent} from "./components/about-us/about-us.component";
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about-us', component: AboutUsComponent},
+  {path: 'our-project', component: AboutUsComponent},
+  {path: 'investors-relations', component: AboutUsComponent},
+  {path: 'news', component: AboutUsComponent},
+  {path: 'gallery', component: AboutUsComponent},
+  {path: 'contact-us', component: AboutUsComponent},
 ];
 
 @NgModule({
